ec6c0ce264548c04dce700bf9dbaaff24d535492
JRice <jrice.blue@gmail.com>
2026-05-19 13:34:07 -0400

    Better safety of kube commands

--- .bashrc.d/82-kube.bash
+++ .bashrc.d/82-kube.bash
@@ -1,9 +1,11 @@
-export KUBECONFIG=~/.kube/eol-developer.kubeconfig
-export SOPS_AGE_KEY_FILE="$HOME/.config/sops/age/keys.txt"
-kubectl config use-context eol-dev
+if command -v kubectl; then
+  export KUBECONFIG=~/.kube/eol-developer.kubeconfig
+  export SOPS_AGE_KEY_FILE="$HOME/.config/sops/age/keys.txt"
+  kubectl config use-context eol-dev
 
-alias kc="kubectl get"
-alias kcc="kubectl config"
-alias kpod="kubectl get pods"
+  alias kc="kubectl get"
+  alias kcc="kubectl config"
+  alias kpod="kubectl get pods"
 
-alias kcontext="kcc get-contexts" # NOT NEEDED: ; kcc current-context"
+  alias kcontext="kcc get-contexts" # NOT NEEDED: ; kcc current-context"
+fi
