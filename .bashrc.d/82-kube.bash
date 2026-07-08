1a584085c0aca1c6c30ebe4c0bf26008b61d8c51
JRice <jrice.blue@gmail.com>
2026-07-08 10:46:36 -0400

    Updating k8s aliases

--- .bashrc.d/82-kube.bash
+++ .bashrc.d/82-kube.bash
@@ -16,19 +16,25 @@ if command -v kubectl >/dev/null 2>&1; then
   alias kstat="ea && kc rollout status deploy/publishing-nginx"
   alias krestart="kc -n eol-staging rollout restart deploy/publishing-web"
 
+  alias kdesc="kc describe pod"
+
   alias kclear="rm -rf ~/.kube/cache/" # Clears the cache.
   alias kcontext="kcc get-contexts"
 
   krc() {
-  kubectl -n eol-staging exec -it deploy/publishing-web -- sh -c '
-    export RAILS_MASTER_KEY=$(cat /run/secrets/publishing/RAILS_MASTER_KEY)
-    export NEO4J_USER=$(cat /run/secrets/publishing/NEO4J_USER)
-    export NEO4J_PASSWORD=$(cat /run/secrets/publishing/NEO4J_PASSWORD)
-    exec bundle exec rails console
+  kc exec -it "deploy/$1" -- sh -c '
+    export RAILS_MASTER_KEY="$(cat "$RAILS_MASTER_KEY_FILE")"
+    export NEO4J_USER="$(cat "$NEO4J_USER_FILE")"
+    export NEO4J_PASSWORD="$(cat "$NEO4J_PASSWORD_FILE")"
+    bundle exec rails c
   '
   }
+
+  alias kwork="krc publishing-worker"
 
   alias kupneosecrets="sops --decrypt overlays/staging/neo4j-secrets.sops.yaml | kc apply -f -"
   alias kuppubsecrets=" sops --decrypt overlays/staging/publishing-secrets.sops.yaml | kc apply -f -"
   alias kupsecrets="kupneosecrets && kuppubsecrets"
+
+  alias comaliask="popd ~ ; git add ~/.bashrc.d/82-kube.bash ; git ci -m 'Updating k8s aliases' ; git push ; popd"
 fi
