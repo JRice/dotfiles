69f26f466f229b49903993f08288f4761ec1d954
JRice <jrice.blue@gmail.com>
2026-07-16 07:42:56 -0400

    Updating k8s aliases based on real work

--- .bashrc.d/82-kube.bash
+++ .bashrc.d/82-kube.bash
@@ -13,13 +13,20 @@ if command -v kubectl >/dev/null 2>&1; then
   alias kcani="kc auth can-i"
 
   alias kapp="ea && kc apply -k publishing/overlays/staging"
+  alias kappprod="ea && kc apply -k publishing/overlays/prod"
   alias kstat="ea && kc rollout status deploy/publishing-nginx"
   alias krestart="kc -n eol-staging rollout restart deploy/publishing-web"
 
   alias kdesc="kc describe pod"
+  alias klog="kc logs"
+  alias klogprev="klog --previous"
+  alias kstat="echo \"NOTE: you'll need to add 'sts/' before the name of the resource (not the pod).\" && echo \"---\" && kc rollout status"
 
   alias kclear="rm -rf ~/.kube/cache/" # Clears the cache.
-  alias kcontext="kcc get-contexts"
+  alias kcontext="kcc get-contexts" # Also kcc current-context, but this is more info.
+  alias kcontextprod="kcc use-context eol-prod"
+  alias kcontextstage="kcc use-context eol-staging"
+  alias ksetcontext="kubectl config use-context"
 
   krc() {
   kc exec -it "deploy/$1" -- sh -c '
